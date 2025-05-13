f23766bbefdf59f5fe1e4551b04ef35f42e54431
Philippe Maia <philippe.m.maia@hotmail.com>
2025-05-13 00:00:26 -0300

    primeiro commit

---
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,49 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Role role)) return false;
+
+        return authority.equals(role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return authority.hashCode();
+    }
+}
